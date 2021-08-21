f9e8df0cdd13c5b4e7812b41bfc36caabf2f121a
Precious Damisa <preciousdamisa@gmail.com>
2021-08-21 05:50:32 +0100

    Update

--- dist/app.js
+++ dist/app.js
@@ -11,7 +11,7 @@ const users_1 = __importDefault(require("./routes/user/users"));
 const auth_1 = __importDefault(require("./routes/user/auth"));
 const wallets_1 = __importDefault(require("./routes/wallets"));
 const app = express_1.default();
-app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
 app.use('/api/users', users_1.default);
 app.use('/api/auth', auth_1.default);
 app.use('/api/wallets', wallets_1.default);
